0abf1b93a5cb6f74fa1904ea1210e67c1171c204
Gustavo <gustavo.goncalvespires@gmail.com>
2020-08-21 17:39:25 -0300

    Printing the Stack

--- src/com/guhpires/datastructures/Stack.java
+++ src/com/guhpires/datastructures/Stack.java
@@ -2,16 +2,44 @@ package com.guhpires.datastructures;
 
 public class Stack<T> {
     LinkedList<T> items = new LinkedList<>();
+    private int size = 0;
+
+    // Returns the Stack size
+    // Time complexity: O(1)
+    public int size() { return this.size; }
+
+    // Returns if the Stack is empty
+    // Time complexity: O(1)
+    public boolean isEmpty() { return size() == 0; }
 
     // Adding element to Stack
     // Time complexity: O(1)
-    public void push(T data) { items.add(data); }
+    public void push(T data) {
+        items.add(data);
+        this.size++;
+    }
 
     // Removing element from Stack
     // Time complexity: O(1)
-    public void pop() { items.remove(); }
+    public void pop() {
+        items.remove();
+        this.size--;
+    }
 
     // Get the element from the top of the Stack
     // Time complexity: O(1)
-    public T peek() { return (T) items.getHead().peek(); }
+    public T peek() {
+        if(isEmpty()) throw new RuntimeException("The Stack is empty.");
+        return (T) items.getHead().peek();
+    }
+
+    public void print() {
+        LinkedList.Node data = items.getHead();
+        while(!isEmpty()) {
+            System.out.println(" | " + data.peek() + " | ");
+            if(data.next() != null) data = data.next();
+            else break;
+        }
+        System.out.println("");
+    }
 }
